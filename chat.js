f9fecddb108ecde8a6d4a7f6574eb6ae55765ca0
unknown <andrewkembo91@gmail.com>
2023-03-17 19:17:02 +0200

    first commit

---
+++ chat.js
@@ -0,0 +1,12 @@
+let batt = document.querySelector("#batt");
+
+batt.addEventListener("click",function(){
+alert("follow us in our facebook")    
+})
+
+
+let bat = document.getElementById("bat");
+
+bat.addEventListener("click",function(){
+        alert("please sign in")
+})
